389d798e1678fc31bbcc14c2c77103fa29baa0f0
Bill Wilfried <billwilfriedfutgnia@outlook.fr>
2020-11-19 11:45:09 +0100

    Revised

---
+++ CC01.BO/Ecole.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    public class Ecole
+    {
+        public string Intitulé { get; set; }
+        public long Téléphone { get; set; }
+        public string Email { get; set; }
+        public byte[] Logo { get; set; }
+
+        public Ecole()
+        {
+        }
+
+        public Ecole(string intitulé, byte[] logo, long téléphone, string email)
+        {
+            Intitulé = intitulé;
+            Logo = logo;
+            Téléphone = téléphone;
+            Email = email;
+        }
+    }
+}
